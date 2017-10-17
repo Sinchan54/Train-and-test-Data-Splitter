d92d782d375af707c9185b971e95a8aca68c7b74
Sinchan54 <sinchan.bhattacharya@mavs.uta.edu>
2017-10-17 14:15:45 -0500

    Update txtFileSplitter.py

--- txtFileSplitter.py
+++ txtFileSplitter.py
@@ -1,14 +1,14 @@
 import random
 
-fin = open("C:/Sinchan/object_recog/alscrap_train.txt", 'rb')
-f75out = open("C:/Sinchan/object_recog/train_alscrap.txt", 'wb')
-f25out = open("C:/Sinchan/object_recog/test_alscrap.txt", 'wb')
+fin = open("Pathname/Main_Filename.txt", 'rb')
+f1out = open("Pathname/Train_Filename.txt", 'wb')
+f2out = open("Pathname/Test_Filename.txt", 'wb')
 for line in fin:
     r = random.random()
-    if r < 0.5:
-        f75out.write(line)
+    if r < 0.7:     #Change this value to decide the ratio you wanna split the files. I am doing it 70%-30%
+        f1out.write(line)
     else:
-        f25out.write(line)
+        f2out.write(line)
 fin.close()
-f75out.close()
-f25out.close()
+f1out.close()
+f2out.close()
